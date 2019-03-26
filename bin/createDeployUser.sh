50f11640b5ab2519e6956b7884accb7a00d9dbe7
Alex <@muzo0@yandex.ru>
2019-03-27 00:21:42 +0300

    fix swarm deploy

--- bin/createDeployUser.sh
+++
@@ -1,9 +0,0 @@
-echo "Create user"
-sudo adduser deployer
-sudo groupadd docker
-sudo usermod -aG docker deployer
-
-echo -e "\necho \"Generate ssh key\"\nssh-keygen -t rsa\ncp ~/.ssh/id_rsa.pub ~/.ssh/authorized_keys\necho \"ssh key\"\ncat ~/.ssh/id_rsa" >> /home/deployer/genereateKey__.sh
-chmod +x /home/deployer/genereateKey__.sh
-su -c "~/genereateKey__.sh" deployer
-rm /home/deployer/genereateKey__.sh
